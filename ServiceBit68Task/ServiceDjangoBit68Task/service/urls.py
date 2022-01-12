8b50cf47b83d1d886ad66fc0808a4e139b0776b9
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2022-01-12 13:56:18 +0200

    make urls and view and make navigate of urls

---
+++ ServiceBit68Task/ServiceDjangoBit68Task/service/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('products/', views.products, name='products'),
+    path('product/<str:pk>/', views.product, name='product'),
+]
