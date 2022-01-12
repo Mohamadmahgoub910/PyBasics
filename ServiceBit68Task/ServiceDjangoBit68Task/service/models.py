a485e4dabdb796d55e55ceab65222d27b4442bb6
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2022-01-13 00:44:28 +0200

    create superuser to add products and let any user add but signup not yet

--- ServiceBit68Task/ServiceDjangoBit68Task/service/models.py
+++ ServiceBit68Task/ServiceDjangoBit68Task/service/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Products(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField( max_length=200)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    createdAt = models.DateField(auto_now_add=True)
+    id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
