d4966f1268e68ca928d293270b477686d5b8ad1e
Mohamadmahgoub910 <mohamadmahgoub1@Outlook.com>
2022-01-14 04:46:48 +0200

    Hacker rank solving some probs

---
+++ HackerRankProblems/basics/Python If-Else.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    inp = int(input().strip())
+    if (inp % 2 == 1) | (6 <= inp <= 20):
+        print("Weird")
+    elif inp >= 2 & inp <= 5:
+        print("Not Weird")
+    elif inp > 20:
+        print()
